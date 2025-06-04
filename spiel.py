75a03d649ba65aa4bad1376bef7ef37ad7a29c8c
Matejastinkt2 <lstarcevic35@gmail.com>
2025-06-04 10:58:45 +0200

    version_004

--- spiel.py
+++ spiel.py
@@ -3,3 +3,27 @@ Mensch ärgere dich nicht
 Ersteller: Simsek Emre | Starcevic Luka | Turek Alexander
 """
 import tkinter as tk
+import random
+
+Hintergrund = 
+
+
+class Spielbrett:
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.startposition = {
+        "rot": [(100,80),(100,100),(120,80),(120,100)],
+        "gruen": [(0,80),(0,100),(20,80),(20,100)],   
+        "gelb": [(0,0),(0,20),(20,0),(20,20)], 
+        "blau": [(100,0),(100,20),(120,0),(120,20)],   
+        }
+    
+    def Hintergrund(self):
+
+
+class Spieler:
+
+class Figur:
+
+class Spiel:
+
