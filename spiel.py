45a3dab19fc04043203794c8fba9e603f6fc2233
Matejastinkt2 <lstarcevic35@gmail.com>
2025-06-11 09:37:36 +0200

    version_008

--- spiel.py
+++ spiel.py
@@ -1,44 +1,31 @@
 """
-Mensch ärgere dich nicht
+Würfelspiel
 Ersteller: Simsek Emre | Starcevic Luka | Turek Alexander
 """
+
 import customtkinter as ctk
-from PIL import Image
 import random
 
-app = ctk.CTk()
-app.title("Mensch ärgere dich nicht")
-app.after(0, lambda: app.state("zoomed"))
+ctk.set_apperance_mode("light")
 
-label = ctk.CTkLabel(app, text="", font=("Arial", 20, "bold"))
-label.pack()
+#Variablen
+max_spieler = 4
+animation_länge = 25
+gewinner_anzeige = 3000
 
-label = ctk.CTkLabel(app, text="Mensch ärgere dich nicht", font=("Arial", 100, "bold"))
-label.pack()
-
-label = ctk.CTkLabel(app, text="", font=("Arial", 20, "bold"))
-label.pack()
-
-"""
-class Spielbrett:
-    def __init__(self, canvas):
-        self.canvas = canvas
-        self.startposition = {
-        "rot": [(100,80),(100,100),(120,80),(120,100)],
-        "gruen": [(0,80),(0,100),(20,80),(20,100)],   
-        "gelb": [(0,0),(0,20),(20,0),(20,20)], 
-        "blau": [(100,0),(100,20),(120,0),(120,20)],   
-        }
-    
-    def hintergrund(self):
-
+#Farben
+hintergrund = "white"
+textfarbe = "black"
 
 class Spieler:
+    def __init__(self, spieler, name):
+        self.name = name
+        self.frame = ctk.CTkFrame(spieler, fg_color = hintergrund, corner_radius = 10, border_width = 2)
+        self.frame.pack(expand = True, side = "left")
+        self.tipp_label = ctk.CTkLabel(self.frame, text = "", font = ("Arial", 24), text_color = textfarbe)
+        self.tipp_label.pack(pady = 5)
+        self.tipp = None
 
-class Figur:
 
-class Spiel:
 
-"""
 
-app.mainloop()
